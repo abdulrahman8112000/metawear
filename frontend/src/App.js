b71a005e4da5a965c6836ec403c8c303cda48cae
Abdul Rahman Hashmi <rehmanabdul281@gmail.com>
2022-05-30 12:15:27 +0500

    message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">metawear</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Featured products</h1>
+        <div className="products">
+        {data.products.map((product) => (
+          <div className="product" key={product.slug}>
+            <img src={product.image} alt={product.name}></img>
+            <p>{product.name}</p>
+            <p>{product.price}</p>
+          </div>
+        ))}
+        </div>
+      </main>
     </div>
   );
 }
